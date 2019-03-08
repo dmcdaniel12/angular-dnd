4ff95497179c5ed855eb39048f1d83a6fff9e58d
Derek McDaniel <derek@rods.com>
2019-03-08 13:29:26 -0500

    Fixed monster stuff

--- src/app/components/monster/monster.component.ts
+++ src/app/components/monster/monster.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Monster } from '../../domain/monsters';
-import { MonstersService } from '../../services/monsters.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Monster} from '../../domain/monsters';
+import {MonstersService} from '../../services/monsters.service';
 
 @Component({
-  selector: 'app-monster',
-  templateUrl: './monster.component.html',
-  styleUrls: ['./monster.component.css']
+    selector: 'app-monster',
+    templateUrl: './monster.component.html',
+    styleUrls: ['./monster.component.css']
 })
 export class MonsterComponent implements OnInit {
-  public monster: Monster
-  private id: any;
+    public monster: Monster;
+    private id: any;
 
-  constructor(
-      private monsterService: MonstersService,
-      private router: Router,
-      private activatedRoute: ActivatedRoute
-  ) {
+    constructor(
+        private monsterService: MonstersService,
+        private router: Router,
+        private activatedRoute: ActivatedRoute
+    ) {
 
-    this.activatedRoute.params
-        .subscribe(params => {
-          if (params.id) {
-            this.id = params.id;
-          } else {
-            // redirect back to monsters
-          }
+        this.activatedRoute.params
+            .subscribe(params => {
+                if (params.id) {
+                    this.id = params.id;
+                } else {
+                    // redirect back to monsters
+                }
+            });
+
+
+    }
+
+    ngOnInit() {
+        this.monsterService.getMonster(this.id).subscribe(monster => {
+            this.monster = monster;
         });
+    }
 
+    public print() {
+        const mywindow = window.open('', '', 'left=0,top=0,width=1280,height=768,toolbar=0,scrollbars=0,status=0');
 
-  }
+        mywindow.document.write(document.getElementById('print').innerHTML);
+        mywindow.document.close(); // necessary for IE >= 10
+        mywindow.focus(); // necessary for IE >= 10*/
+        mywindow.print();
+        mywindow.close();
 
-  ngOnInit() {
-    this.monsterService.getMonster(this.id).subscribe(monster => { this.monster = monster; });
-  }
+        return true;
+    }
 
 }
